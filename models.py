fbf90fe9aefb4b3325910c209bd62d8d8556e22b
Daniel Brito <danielbrito@pq.uenf.br>
2023-05-28 15:23:52 -0300

    feat: draft timetablling models

--- models.py
+++ models.py
@@ -3,9 +3,9 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import ForeignKey
 
 from database import Base
-
 
 class Todo(Base):
     """ Todo Class """
@@ -14,4 +14,34 @@ class Todo(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(30))
     complete: Mapped[bool] = mapped_column(Boolean, default=False)
-# schemas?
+
+class Professor(Base):
+    """ Professor Class """
+    __tablename__ = "professor"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(30))
+
+class Disciplina(Base):
+    """ Disciplina Class"""
+    __tablename__ = "disciplina"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(30))
+    code: Mapped[str] = mapped_column(String(9))
+
+class Turma(Base):
+    """ Turma Class"""
+    __tablename__ = "turma"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    disciplina_id: Mapped[int] = mapped_column(ForeignKey("disciplina.id"))
+    professor_id: Mapped[int] = mapped_column(ForeignKey("professor.id"))
+
+class TimeSlot(Base):
+    """ Timeslot Class"""
+    __tablename__ = "time_slot"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    turma_id: Mapped[int] = mapped_column(ForeignKey("turma.id"))
+    slot: Mapped[str] = mapped_column(Integer) #TODO: consider monA, tueC || AA, BC || 0, 10 
