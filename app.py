9ce94c8cd70100a5a1181e2594ff6e50350c4750
Daniel Brito <danielbrito@pq.uenf.br>
2023-05-28 15:41:55 -0300

    refactor: move todo to /todo

--- app.py
+++ app.py
@@ -26,42 +26,50 @@ def get_db():
     finally:
         db.close()
 
-
 @app.get("/")
-def home(request: Request, db: Session = Depends(get_db)):
+def home():
     """ Home """
+    return "Test"
+
+
+
+
+
+@app.get("/todo")
+def todo_home(request: Request, db: Session = Depends(get_db)):
+    """ Todo Home """
     todos = db.query(models.Todo).all()
     return templates.TemplateResponse("base.html",
                                       {"request": request, "todo_list": todos})
 
 
-@app.post("/add")
-def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+@app.post("/todo/add")
+def todo_add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
     new_todo = models.Todo(title=title)
     db.add(new_todo)
     db.commit()
 
-    url = app.url_path_for("home")
+    url = app.url_path_for("todo_home")
     return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
 
 
-@app.get("/update/{todo_id}")
-def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
+@app.get("/todo/update/{todo_id}")
+def todo_update(request: Request, todo_id: int, db: Session = Depends(get_db)):
     """ Update """
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     todo.complete = not todo.complete
     db.commit()
 
-    url = app.url_path_for("home")
+    url = app.url_path_for("todo_home")
     return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
 
 
-@app.get("/delete/{todo_id}")
-def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+@app.get("/todo/delete/{todo_id}")
+def todo_delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
     """ Delete """
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     db.delete(todo)
     db.commit()
 
-    url = app.url_path_for("home")
+    url = app.url_path_for("todo_home")
     return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
